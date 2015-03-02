479c1b950efd6bfa4f26e4b1b818189db59cc9ae
mikemags1 <mikemags1@gmail.com>
2015-03-02 16:06:20 -0600

    now finds views for submission metrics in onCreate (radiogroups checkboxes, etc)

--- app/src/main/java/com/outfitterandroid/SubmissionActivity.java
+++ app/src/main/java/com/outfitterandroid/SubmissionActivity.java
@@ -3,7 +3,10 @@ package com.outfitterandroid;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 
 import java.io.ByteArrayOutputStream;
 
@@ -17,6 +20,10 @@ public class SubmissionActivity extends Activity {
     private Bitmap mPhotoBitmap;
 
     private ImageView mSubmissionPhoto;
+    private RadioGroup mArticleRadioGroup;
+    private RadioGroup mAudienceRadioGroup;
+    private CheckBox mIsPriorityCheckBox;
+    private Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,10 @@ public class SubmissionActivity extends Activity {
         setContentView(R.layout.activity_submission);
 
         mSubmissionPhoto = (ImageView) findViewById(R.id.submission_photo_view);
+        mArticleRadioGroup = (RadioGroup) findViewById(R.id.article_selection_radio_group);
+        mAudienceRadioGroup = (RadioGroup) findViewById(R.id.audience_selection_radio_group);
+        mIsPriorityCheckBox = (CheckBox) findViewById(R.id.is_priority_submission_check_box);
+        mSubmitButton = (Button) findViewById(R.id.submit_submission_button);
 
         Bitmap photoBitmap = (Bitmap) getIntent()
                 .getBundleExtra(ProfileActivity.EXTRA_SUBMISSION_BUNDLE)
