e8831a68c382fa9893ec2eef6c2df5168598a3b1
mikemags1 <mikemags1@gmail.com>
2015-03-02 21:50:15 -0600

    fixed hasSubmittedPrioritySubmissionToday logic fixed updateLastPiroritySubmission to now save, was not before added addToSubmissionsList method for updating user submission list during a submission

--- app/src/main/java/com/outfitterandroid/User.java
+++ app/src/main/java/com/outfitterandroid/User.java
@@ -1,15 +1,14 @@
 package com.outfitterandroid;
 
 import android.graphics.Bitmap;
+import android.util.Log;
 
-import com.parse.GetCallback;
-import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -30,23 +29,24 @@ public class User {
         c.set(GregorianCalendar.HOUR_OF_DAY, 0);
         c.set(GregorianCalendar.MINUTE, 0);
         c.set(GregorianCalendar.SECOND, 0);
+        c.set(GregorianCalendar.MILLISECOND, 0);
 
-        if(null == lastPrioritySubmissionDate){
-            return true;
-        }
-        else
-        {
-            if (lastPrioritySubmissionDate.compareTo(c.getTime()) < 0){
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
+        Log.d("in func", Boolean.toString(lastPrioritySubmissionDate == null ?  false : lastPrioritySubmissionDate.compareTo(c.getTime()) <= 0));
+
+        if(lastPrioritySubmissionDate == null) return false;
+        if(lastPrioritySubmissionDate.compareTo(c.getTime()) <= 0) return false;
+        return true;
     }
 
+    /**
+     * submits the submission to the submission table in Parse. Also updates user's list
+     * of submission object ids to include this submission
+     * @param user user submitting the submission
+     * @param mCurrentSubmission submission info to be prepared and submitted
+     */
     public static void submitSubmission(ParseUser user, Submission mCurrentSubmission) {
         ParseObject submission = new ParseObject("Submission");
+        submission.put("submittedByUser", mCurrentSubmission.getSubmittedByUser());
         submission.put("article", mCurrentSubmission.getArticle());
         submission.put("numLikes", mCurrentSubmission.getNumLikes());
         submission.put("numDislikes", mCurrentSubmission.getNumDislikes());
@@ -64,6 +64,9 @@ public class User {
         submission.put("image", image);
         submission.saveInBackground();
 
+        //add to user's submissions
+        addToSubmissionsList(user, submission.getObjectId());
+
         if(mCurrentSubmission.isPrioritySubmission()){
             User.updateLastPrioritySubmission(user);
         }
@@ -71,25 +74,32 @@ public class User {
 
     private static void updateLastPrioritySubmission(ParseUser user) {
         String currentUserObjectId = user.getObjectId();
+        user.put("lastPrioritySubmission", new Date());
+        user.saveInBackground();
+    }
 
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+    private static void addToSubmissionsList(ParseUser user, String submissionObjectId){
+        ArrayList<String> curSubmissionIDList = (ArrayList) user.getList("submissionIDList");
+        if(curSubmissionIDList == null) curSubmissionIDList = new ArrayList<>();
+        curSubmissionIDList.add(submissionObjectId);
+        user.put("submissionIDList", curSubmissionIDList);
+        user.saveInBackground();
+    }
 
 
-        query.getInBackground(currentUserObjectId, new GetCallback<ParseObject>() {
-            @Override
-            public void done(ParseObject parseObject, ParseException e) {
-                parseObject.put("lastPrioritySubmission", new Date());
-                parseObject.saveInBackground();
-            }
-        });
+    /**
+     * deletes user account
+     * @param user user to be deleted
+     */
+    public static void deleteUser(ParseUser user) {
+        user.deleteInBackground();
     }
 
     /**
-     * removes user and all submissions by user
-     * @param user user to be deleted
+     * deletes all associated submissions by the user
+     * @param user
      */
-    public static void delete(ParseUser user) {
+    public static void deleteAssociatedData (ParseUser user){
         //to be implemented
-        user.deleteInBackground();
     }
 }
