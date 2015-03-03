c664157a5ea05fdbbb293f3a8da9a7ea2d930bdf
mikemags1 <mikemags1@gmail.com>
2015-03-02 21:14:52 -0600

    radiobutton group listeners added checkbox literner added submit button listener added
    submission working preliminarily

--- app/src/main/java/com/outfitterandroid/SubmissionActivity.java
+++ app/src/main/java/com/outfitterandroid/SubmissionActivity.java
@@ -3,23 +3,32 @@ package com.outfitterandroid;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
+import com.parse.ParseUser;
+
+import java.util.Date;
 
 /**
  * Created by MaguireM on 2/23/15.
  */
 public class SubmissionActivity extends Activity {
     private static final String TAG = "SubmissionActivity";
+    private static final String ALREADY_SUBMITTED_PRIORITY_SUBMISSION_TODAY = "Already submitted priority submission today!";
 
-    private String mCurrentPhotoPath;
+    //private String mCurrentPhotoPath;
     private Bitmap mPhotoBitmap;
+    private Submission mCurrentSubmission;
+    private ParseUser mCurrentUser;
 
-    private ImageView mSubmissionPhoto;
+    private ImageView mSubmissionImageView;
     private RadioGroup mArticleRadioGroup;
     private RadioGroup mAudienceRadioGroup;
     private CheckBox mIsPriorityCheckBox;
@@ -30,18 +39,83 @@ public class SubmissionActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
 
-        mSubmissionPhoto = (ImageView) findViewById(R.id.submission_photo_view);
+        mSubmissionImageView = (ImageView) findViewById(R.id.submission_image_view);
         mArticleRadioGroup = (RadioGroup) findViewById(R.id.article_selection_radio_group);
         mAudienceRadioGroup = (RadioGroup) findViewById(R.id.audience_selection_radio_group);
         mIsPriorityCheckBox = (CheckBox) findViewById(R.id.is_priority_submission_check_box);
         mSubmitButton = (Button) findViewById(R.id.submit_submission_button);
 
-        Bitmap photoBitmap = (Bitmap) getIntent()
+        mCurrentUser = ParseUser.getCurrentUser();
+
+        //set image view and checkboxes
+
+        Bitmap capturedImage = (Bitmap) getIntent()
                 .getBundleExtra(ProfileActivity.EXTRA_SUBMISSION_BUNDLE)
                 .get("data");
-        mSubmissionPhoto.setImageBitmap(photoBitmap);
-//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//        photoBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-//        byte[] photoByteArray = stream.toByteArray();
+        mSubmissionImageView.setImageBitmap(capturedImage);
+        mIsPriorityCheckBox.setChecked(!User.hasSubmittedPrioritySubmissionToday(mCurrentUser));
+
+        //build submission
+
+        mCurrentSubmission = new Submission();
+        mCurrentSubmission.setIsPrioritySubmission(!User.hasSubmittedPrioritySubmissionToday(mCurrentUser));
+        //mCurrentSubmission.addImage(capturedImage);
+        mCurrentSubmission.setImage(capturedImage);
+
+        //set button listeners
+
+        mArticleRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                mCurrentSubmission.setArticle(group.indexOfChild(findViewById(checkedId)) - 1);
+            }
+        });
+
+        mAudienceRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (group.indexOfChild(findViewById(checkedId))){
+                    case 1:
+                        mCurrentSubmission.setToReceiveMaleFeedback(true);
+                        mCurrentSubmission.setToReceiveFemaleFeedback(true);
+                        break;
+                    case 2:
+                        mCurrentSubmission.setToReceiveMaleFeedback(true);
+                        mCurrentSubmission.setToReceiveFemaleFeedback(false);
+                        break;
+                    case 3:
+                        mCurrentSubmission.setToReceiveMaleFeedback(false);
+                        mCurrentSubmission.setToReceiveFemaleFeedback(true);
+                        break;
+                }
+            }
+        });
+
+        mIsPriorityCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d("wtwtf", Boolean.toString(User.hasSubmittedPrioritySubmissionToday(mCurrentUser)));
+                if(isChecked && User.hasSubmittedPrioritySubmissionToday(mCurrentUser)){
+                    Toast.makeText(SubmissionActivity.this, ALREADY_SUBMITTED_PRIORITY_SUBMISSION_TODAY, Toast.LENGTH_SHORT)
+                            .show();
+                    buttonView.setChecked(false);
+                }
+                else
+                {
+                    mCurrentSubmission.setIsPrioritySubmission(isChecked);
+                }
+            }
+        });
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User.submitSubmission(mCurrentUser, mCurrentSubmission);
+                setResult(RESULT_OK);
+                finish();
+            }
+        });
+
+
     }
 }
