0e8fe78c62701b3e92c851fdc56e9947332a6f33
sly117 <kkocinski@gmail.com>
2015-04-05 12:58:36 -0500

    Implemented date, likes and dislikes tests. All are passing.

--- app/src/androidTest/java/com/outfitterandroid/unittests/SubmissionTest.java
+++ app/src/androidTest/java/com/outfitterandroid/unittests/SubmissionTest.java
@@ -1,6 +1,10 @@
 package com.outfitterandroid.unittests;
 
+import com.outfitterandroid.Submission;
+
 import junit.framework.TestCase;
+
+import java.util.Date;
 
 /**
  * Tests for submission
@@ -8,15 +12,25 @@ import junit.framework.TestCase;
 public class SubmissionTest extends TestCase {
 
     public void testGetCreatedAt() throws Exception {
-
+        Submission testSub = new Submission();
+        Date testDate = new Date();
+        testDate.setTime(1000000);
+        testSub.setCreatedAt(testDate);
+        assertEquals(testDate, testSub.getCreatedAt());
     }
 
     public void testGetNumLikes() throws Exception {
-
+        Submission testSub = new Submission();
+        int numLikes = 100;
+        testSub.setNumLikes(numLikes);
+        assertEquals(numLikes, testSub.getNumLikes());
     }
 
     public void testGetNumDislikes() throws Exception {
-
+        Submission testSub = new Submission();
+        int numDislikes = 100;
+        testSub.setNumDislikes(numDislikes);
+        assertEquals(numDislikes, testSub.getNumDislikes());
     }
 
     public void testGetArticle() throws Exception {
