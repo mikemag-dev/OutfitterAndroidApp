c091bb91c250570457bdbcfb0be9ccd6d8e24f7b
sly117 <kkocinski@gmail.com>
2015-04-05 12:49:59 -0500

    Added a SubmissionActivityTest for future use

---
+++ app/src/androidTest/java/com/outfitterandroid/unittests/SubmissionActivityTest.java
@@ -0,0 +1,15 @@
+package com.outfitterandroid.unittests;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import com.outfitterandroid.SubmissionActivity;
+
+import junit.framework.TestCase;
+
+public class SubmissionActivityTest extends TestCase {
+
+    //TODO Tests If needed
+
+}
