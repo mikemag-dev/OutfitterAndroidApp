e66db5ba09f02913c79167d1e951a850869ef0a8
maguire7 <mikemags1@gmail.com>
2015-04-08 17:44:36 -0500

    swipe in discovery mode working

--- app/src/main/java/com/outfitterandroid/DiscoveryActivity.java
+++ app/src/main/java/com/outfitterandroid/DiscoveryActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.MotionEventCompat;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,6 +26,9 @@ import java.util.ArrayList;
  */
 public class DiscoveryActivity extends Activity{
 
+
+    GestureDetectorCompat mGestureDetector;
+
     ImageView mSubmissionImageView;
     Button mLikeButton;
     Button mDislikeButton;
@@ -32,14 +40,22 @@ public class DiscoveryActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discovery);
 
+        mGestureDetector = new GestureDetectorCompat(this, new SwipeGestureListener());
+
         mSubmissionImageView = (ImageView) findViewById(R.id.discovery_image_view);
         mLikeButton = (Button) findViewById(R.id.discovery_like_button);
         mDislikeButton = (Button) findViewById(R.id.discovery_dislike_button);
 
         mLikeButton.setOnClickListener(submitVote());
         mDislikeButton.setOnClickListener(submitVote());
+        mSubmissionImageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                mGestureDetector.onTouchEvent(event);
+                return false;
+            }
+        });
     }
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -72,6 +88,13 @@ public class DiscoveryActivity extends Activity{
         }
     }
 
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mGestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
     private View.OnClickListener submitVote() {
         return new View.OnClickListener() {
             @Override
@@ -94,4 +117,31 @@ public class DiscoveryActivity extends Activity{
             }
         };
     }
+    class SwipeGestureListener extends GestureDetector.SimpleOnGestureListener{
+        private static final String TAG = "Gestures";
+
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            boolean liked;
+            if(velocityX > 0){
+                liked = true;
+                User.submitVote(ParseUser.getCurrentUser(), mCurrentSubmission, liked);
+                populateWithSubmission();
+                Log.d(TAG, "right swipe detected, submitted like vote");
+                return true;
+            }
+            else if (velocityX < 0){
+                liked = false;
+                User.submitVote(ParseUser.getCurrentUser(), mCurrentSubmission, liked);
+                populateWithSubmission();
+                Log.d(TAG, "left swipe detected, submitted dislike vote");
+                return true;
+            }
+            else{
+                return super.onFling(e1, e2, velocityX, velocityY);
+            }
+        }
+    }
+
 }
