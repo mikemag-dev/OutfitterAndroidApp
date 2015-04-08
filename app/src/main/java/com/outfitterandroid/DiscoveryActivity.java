a18c7af5408dc32bdb12876aa2c1033852a5c747
maguire7 <mikemags1@gmail.com>
2015-04-08 18:15:02 -0500

    - refactored submission listeners - added private variables for article and gender submmited by spinners

--- app/src/main/java/com/outfitterandroid/DiscoveryActivity.java
+++ app/src/main/java/com/outfitterandroid/DiscoveryActivity.java
@@ -5,13 +5,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
-import android.support.v4.view.MotionEventCompat;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.parse.ParseException;
@@ -32,6 +32,8 @@ public class DiscoveryActivity extends Activity{
     ImageView mSubmissionImageView;
     Button mLikeButton;
     Button mDislikeButton;
+    Spinner mArticleSpinner;
+    Spinner mGenderSubmittedBy;
 
     ParseObject mCurrentSubmission;
 
@@ -45,17 +47,24 @@ public class DiscoveryActivity extends Activity{
         mSubmissionImageView = (ImageView) findViewById(R.id.discovery_image_view);
         mLikeButton = (Button) findViewById(R.id.discovery_like_button);
         mDislikeButton = (Button) findViewById(R.id.discovery_dislike_button);
+        mArticleSpinner = (Spinner) findViewById(R.id.article_spinner);
+        mGenderSubmittedBy = (Spinner) findViewById(R.id.gender_submitted_by_spinner);
 
-        mLikeButton.setOnClickListener(submitVote());
-        mDislikeButton.setOnClickListener(submitVote());
-        mSubmissionImageView.setOnTouchListener(new View.OnTouchListener() {
+        mLikeButton.setOnClickListener(submitButtonVote());
+        mDislikeButton.setOnClickListener(submitButtonVote());
+        mSubmissionImageView.setOnTouchListener(submitSwipeVote());
+    }
+
+    private View.OnTouchListener submitSwipeVote() {
+        return new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 mGestureDetector.onTouchEvent(event);
                 return false;
             }
-        });
+        };
     }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -95,7 +104,7 @@ public class DiscoveryActivity extends Activity{
         return super.onTouchEvent(event);
     }
 
-    private View.OnClickListener submitVote() {
+    private View.OnClickListener submitButtonVote() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
