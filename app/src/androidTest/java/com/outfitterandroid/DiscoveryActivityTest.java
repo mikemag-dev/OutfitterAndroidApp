79c5fb1f39eedf89b5049f1839cd5b27e1bcd01a
sly117 <kkocinski@gmail.com>
2015-04-09 22:15:35 -0500

    Added stubs for filter tests. and implemented one of them

--- app/src/androidTest/java/com/outfitterandroid/DiscoveryActivityTest.java
+++ app/src/androidTest/java/com/outfitterandroid/DiscoveryActivityTest.java
@@ -8,12 +8,16 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import com.parse.ParseUser;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 
 public class DiscoveryActivityTest extends ActivityInstrumentationTestCase2 {
 
@@ -31,7 +35,6 @@ public class DiscoveryActivityTest extends ActivityInstrumentationTestCase2 {
             ParseUser.logOut();
         }
         getActivity();
-
     }
 
     public void testDiscoveryOpen() throws InterruptedException {
@@ -81,10 +84,53 @@ public class DiscoveryActivityTest extends ActivityInstrumentationTestCase2 {
         onView(withId(R.id.login_password_input)).perform(typeText(VALID_PASSWORD));
         onView(withId(R.id.parse_login_button)).perform(click());
         Thread.sleep(2000);
-        onView(withId(R.id.profile_discovery_button)).check(matches(withText("Discovery")));
         onView(withId(R.id.profile_discovery_button)).perform(click());
         onView(withId(R.id.discovery_like_button)).check(matches(withText("Like")));
         onView((withId(R.id.discovery_like_button))).perform(click());
         onView(withId(R.id.discovery_like_button)).check(matches(withText("Like")));
     }
+
+    //Filter test below
+    public void testDiscoverFilterGuysGirls() throws InterruptedException {
+        onView(withId(R.id.login_username_input)).perform(typeText(VALID_USERNAME));
+        onView(withId(R.id.login_password_input)).perform(typeText(VALID_PASSWORD));
+        onView(withId(R.id.parse_login_button)).perform(click());
+        Thread.sleep(2000);
+        onView(withId(R.id.profile_discovery_button)).perform(click());
+        onView(withId(R.id.gender_submitted_by_spinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Guys & Girls"))).perform(click());
+        onView(withText("Guys & Girls")).check(matches(withText("Guys & Girls")));
+    }
+/**
+    public void testDiscoverFilterGuys(){
+
+    }
+
+    public void testDiscoverFilterGirls(){
+
+    }
+
+    public void testDiscoverFilterAnyOutfit(){
+
+    }
+
+    public void testDiscoverFilterFullOutfit(){
+
+    }
+
+    public void testDiscoverFilterTopOutfit(){
+
+    }
+
+    public void testDiscoverFilterBottomOutfit(){
+
+    }
+
+    public void testDiscoverFilterShoesOutfit(){
+
+    }
+
+    public void testDiscoverFilterAccessoryOutfit(){
+
+    }*/
 }
