7f12e78e98d1912775d08ea709a20c0aac8ea9f0
sly117 <kkocinski@gmail.com>
2015-04-24 01:32:32 -0500

    12 tests done

--- app/src/androidTest/java/com/outfitterandroid/unittests/SubmissionStatsTest.java
+++ app/src/androidTest/java/com/outfitterandroid/unittests/SubmissionStatsTest.java
@@ -7,45 +7,50 @@ import junit.framework.TestCase;
 public class SubmissionStatsTest extends TestCase {
 
     public void testConstructor(){
+        String testID = "1";
         String testURL = "test_url";
         String testNumLikes = "5";
         String testNumDislikes = "4";
-        SubmissionStats testStats = new SubmissionStats(testURL, testNumLikes, testNumDislikes);
+        SubmissionStats testStats = new SubmissionStats(testID, testURL, testNumLikes, testNumDislikes);
         assertNotNull(testStats);
     }
 
     public void testGetUrl(){
+        String testID = "1";
         String testURL = "test_url";
         String testNumLikes = "5";
         String testNumDislikes = "4";
-        SubmissionStats testStats = new SubmissionStats(testURL, testNumLikes, testNumDislikes);
+        SubmissionStats testStats = new SubmissionStats(testID, testURL, testNumLikes, testNumDislikes);
         String resultUrl = testStats.getUrl();
         assertEquals(testURL, resultUrl);
     }
 
     public void testGetNumLikes(){
+        String testID = "1";
         String testURL = "test_url";
         String testNumLikes = "5";
         String testNumDislikes = "4";
-        SubmissionStats testStats = new SubmissionStats(testURL, testNumLikes, testNumDislikes);
+        SubmissionStats testStats = new SubmissionStats(testID, testURL, testNumLikes, testNumDislikes);
         String resultNumLikes = testStats.getNum_likes();
         assertEquals(testNumLikes, resultNumLikes);
     }
 
     public void testGetNumDislikes(){
+        String testID = "1";
         String testURL = "test_url";
         String testNumLikes = "5";
         String testNumDislikes = "4";
-        SubmissionStats testStats = new SubmissionStats(testURL, testNumLikes, testNumDislikes);
+        SubmissionStats testStats = new SubmissionStats(testID, testURL, testNumLikes, testNumDislikes);
         String resultNumDislikes = testStats.getNum_dislikes();
         assertEquals(testNumDislikes, resultNumDislikes);
     }
 
     public void testUrlNotNull(){
+        String testID = "1";
         String testURL = "test_url";
         String testNum_likes = "5";
         String testNum_dislikes = "4";
-        SubmissionStats testStats = new SubmissionStats(testURL, testNum_likes, testNum_dislikes);
+        SubmissionStats testStats = new SubmissionStats(testID, testURL, testNum_likes, testNum_dislikes);
         String resultUrl = testStats.getUrl();
         assertNotNull(resultUrl);
     }
