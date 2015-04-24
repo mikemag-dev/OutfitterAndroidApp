631044fefcad5849f3255697822552783f1e5b81
sly117 <kkocinski@gmail.com>
2015-04-24 00:24:34 -0500

    Unit Test stubs for Comments

---
+++ app/src/androidTest/java/com/outfitterandroid/unittests/CommentTest.java
@@ -0,0 +1,23 @@
+package com.outfitterandroid.unittests;
+
+/**
+ * Created by Kris on 4/23/2015.
+ */
+
+import junit.framework.TestCase;
+
+
+public class CommentTest extends TestCase{
+
+    public void testGetComments(){
+
+    }
+
+    public void testSortComments(){
+
+    }
+
+    public void testGetNumUpvotes(){
+
+    }
+}
