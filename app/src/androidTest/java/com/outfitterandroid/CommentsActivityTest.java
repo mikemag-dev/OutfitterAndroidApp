0e72b17869e0022d4230f19598f539ac8d5c7b25
sly117 <kkocinski@gmail.com>
2015-04-24 00:53:13 -0500

    Comments Activity test

---
+++ app/src/androidTest/java/com/outfitterandroid/CommentsActivityTest.java
@@ -0,0 +1,19 @@
+package com.outfitterandroid;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by Kris on 4/24/2015.
+ */
+
+public class CommentsActivityTest extends ActivityInstrumentationTestCase2 {
+
+    private static final String VALID_USERNAME = "i_am_a_user\n";
+    private static final String VALID_PASSWORD = "i_am_a_user\n";
+
+    public CommentsActivityTest() {
+        super(LoginDispatchActivity.class);
+    }
+
+
+}
