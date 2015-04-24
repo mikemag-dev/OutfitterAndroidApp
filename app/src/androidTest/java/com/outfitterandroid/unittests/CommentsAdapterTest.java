f9e7f870a3e12066482f6ee2838fcf39e761558f
sly117 <kkocinski@gmail.com>
2015-04-24 00:25:38 -0500

    Unit tests stubs for comment adapter

---
+++ app/src/androidTest/java/com/outfitterandroid/unittests/CommentsAdapterTest.java
@@ -0,0 +1,32 @@
+package com.outfitterandroid.unittests;
+
+/**
+ * Created by Kris on 4/23/2015.
+ */
+
+import junit.framework.TestCase;
+
+public class CommentsAdapterTest extends TestCase {
+
+    public void testConstructor(){
+
+    }
+
+    public void testGetCount(){
+
+    }
+
+    public void testGetItem(){
+
+    }
+
+    public void testGetItemId(){
+
+    }
+
+    public void testHasNotVoted(){
+
+    }
+
+
+}
